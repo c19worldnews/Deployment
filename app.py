7cddf276da0293a25df8bcba00c8ae16a44bd985
Neerja Jhingan <jhinganneerja@gmail.com>
2021-12-12 06:32:47 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+
+
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
