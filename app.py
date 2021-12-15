cccf2e0bfba9875a424a16897aef4d80f9ff7762
Neerja Jhingan <jhinganneerja@gmail.com>
2021-12-15 11:50:59 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--use-fake-ui-for-media-stream")
 
 
 
