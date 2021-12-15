ee3d3a3120b9f5eb69bc3e4ae5eb8474cab7cc32
Neerja Jhingan <jhinganneerja@gmail.com>
2021-12-15 10:59:51 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -126,8 +126,8 @@ def prediction(city_name,country_name,iso_code,label_alpha_2,select_location):
   #return select_location
   #wd = webdriver.Chrome('/usr/bin/chromedriver',options=chrome_options)
   #wd.get("https://share.streamlit.io/")
-  wd = webdriver.Chrome()
-  #wd = webdriver.Chrome('chromedriver',options=options)
+  #wd = webdriver.Chrome()
+  wd = webdriver.Chrome(executable_path ='chromedriver',options=chrome_options)
   #wd = webdriver.Chrome(executable_path ='chromedriver')
   
 
